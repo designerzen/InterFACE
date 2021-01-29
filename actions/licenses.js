c8f9f775a86a50d2e4402a422d42a01a8ebc2531
designerzen <designerzen@gmail.com>
2021-01-29 21:28:25 +0000

    Moved scripts to actions/

---
+++ actions/licenses.js
@@ -0,0 +1,5 @@
+// Originally I had planned on usingf this to extract
+// the licenses from JS and pt them into anchor links
+// but having resad the licenses deeper, I don't
+// think legally I am allowed to do that so instead
+// 300k of license data is loaded and hidden in scripts!
