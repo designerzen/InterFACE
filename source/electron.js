31fb219467aeadd34ad7c3daf09ae0f89cd3ce1c
designerzen <designerzen@gmail.com>
2021-05-04 20:37:40 +0100

    Stripped out gadgets from css

---
+++ source/electron.js
@@ -0,0 +1,146 @@
+// TODO : Implement Electron backend
+
+
+// This is main process of Electron, started as first thing when your
+// app starts. It runs through entire life of your application.
+// It doesn't have any windows which you can see on screen, but we can open
+// window from here.
+
+import path from "path"
+import url from "url"
+import { app, Menu } from "electron"
+import { devMenuTemplate } from "./menu/dev_menu_template"
+import { editMenuTemplate } from "./menu/edit_menu_template"
+
+// Special module holding environment variables which you declared
+// in config/env_xxx.json file.
+import env from "env"
+
+// This helper remembers the size and position of your windows (and restores
+// them in that place after app relaunch).
+// Can be used for more than one window, just construct many
+// instances of
+
+import { app, BrowserWindow, screen } from "electron";
+import jetpack from "fs-jetpack";
+
+const createWindow =(name, options) => {
+  const userDataDir = jetpack.cwd(app.getPath("userData"));
+  const stateStoreFile = `window-state-${name}.json`;
+  const defaultSize = {
+    width: options.width,
+    height: options.height
+  };
+  let state = {};
+  let win;
+
+  const restore = () => {
+    let restoredState = {};
+    try {
+      restoredState = userDataDir.read(stateStoreFile, "json");
+    } catch (err) {
+      // For some reason json can't be read (might be corrupted).
+      // No worries, we have defaults.
+    }
+    return Object.assign({}, defaultSize, restoredState);
+  };
+
+  const getCurrentPosition = () => {
+    const position = win.getPosition();
+    const size = win.getSize();
+    return {
+      x: position[0],
+      y: position[1],
+      width: size[0],
+      height: size[1]
+    };
+  };
+
+  const windowWithinBounds = (windowState, bounds) => {
+    return (
+      windowState.x >= bounds.x &&
+      windowState.y >= bounds.y &&
+      windowState.x + windowState.width <= bounds.x + bounds.width &&
+      windowState.y + windowState.height <= bounds.y + bounds.height
+    );
+  };
+
+  const resetToDefaults = () => {
+    const bounds = screen.getPrimaryDisplay().bounds;
+    return Object.assign({}, defaultSize, {
+      x: (bounds.width - defaultSize.width) / 2,
+      y: (bounds.height - defaultSize.height) / 2
+    });
+  };
+
+  const ensureVisibleOnSomeDisplay = windowState => {
+    const visible = screen.getAllDisplays().some(display => {
+      return windowWithinBounds(windowState, display.bounds);
+    });
+    if (!visible) {
+      // Window is partially or fully not visible now.
+      // Reset it to safe defaults.
+      return resetToDefaults();
+    }
+    return windowState;
+  };
+
+  const saveState = () => {
+    if (!win.isMinimized() && !win.isMaximized()) {
+      Object.assign(state, getCurrentPosition());
+    }
+    userDataDir.write(stateStoreFile, state, { atomic: true });
+  };
+
+  state = ensureVisibleOnSomeDisplay(restore());
+
+  win = new BrowserWindow(Object.assign({}, options, state));
+
+  win.on("close", saveState);
+
+  return win;
+};
+
+const setApplicationMenu = () => {
+  const menus = [editMenuTemplate];
+  if (env.name !== "production") {
+    menus.push(devMenuTemplate);
+  }
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
+};
+
+// Save userData in separate folders for each environment.
+// Thanks to this you can use production and development versions of the app
+// on same machine like those are two separate apps.
+if (env.name !== "production") {
+  const userDataPath = app.getPath("userData");
+  app.setPath("userData", `${userDataPath} (${env.name})`);
+}
+
+app.on("ready", () => {
+  setApplicationMenu();
+
+  const mainWindow = createWindow("main", {
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "app.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  if (env.name === "development") {
+    mainWindow.openDevTools();
+  }
+});
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
