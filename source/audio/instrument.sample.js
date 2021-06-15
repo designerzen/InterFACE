c12d97ddcad4ef3f5abb1eb24d1d300914f31d02
designerzen <designerzen@gmail.com>
2021-06-15 01:09:38 +0100

    feat: Beginnings of instrument Interface

--- source/audio/instrument.sample.js
+++ source/audio/instrument.sample.js
@@ -2,6 +2,18 @@ export default class SampleInstrument extends Instrument{
 
 	constructor( channel="all" ){
 		super()
+		// we add a few extra sample places for the instruments
+		this.polyphony = 5
+	}
+
+	noteOn(note){
+		// playTrack = (audioBuffer, offset=0, destination=delayNode, options={ loop:false } )
+		const track = playTrack( note, 0, this.outputNode ).then( ()=>{
+			this.active = false
+			this.polyphony--
+			//console.log("Sample completed playback... request tock", this.tracks )
+		})
+		super.noteOn(note)
 	}
 
 }
