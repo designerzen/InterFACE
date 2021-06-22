a58fe0d273d292a560be700cb14dd2587cb43db4
designerzen <designerzen@gmail.com>
2021-06-22 14:16:02 +0100

    feat: Added new eye styles and eyeball toggle

--- source/visual/2d.eyes.js
+++ source/visual/2d.eyes.js
@@ -3,10 +3,15 @@ import { clamp, TAU } from "../maths/maths"
 import PALETTE, { DEFAULT_COLOURS } from "../palette"
 
 const DEFAULT_OPTIONS = {
+	// colourful part of the eye
 	iris:"rgba(100,255,100,0.8)", 
 	irisRadius : 1,
+	// holes in the eyes
 	pupil:"rgba(0,0,0,0.8)",
-	pupilRadius:0.3
+	pupilRadius:0.3,
+	// big white bit of the eye
+	sclera:'white',
+	scleraRadius:4
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -21,12 +26,14 @@ export const drawEye = ( eyeData, isLeft=true, open=true, options=DEFAULT_OPTION
 	const down = eyeData[4]
 	const irisWidth = Math.abs(outer[0] - inner[0] )
 	const irisHeight = down[1] - up[1]
-	
+	const showRatio = options.ratio || 0.8 
+
 	// const irisHeight = Math.abs( up[1] - down[1])
 	const diameter = Math.max(irisWidth , irisHeight)
 	const radius = diameter * 0.5
 	
-options = { ...DEFAULT_OPTIONS, options }
+	// you can remove this for speed reasons if you are providing a full options config
+	// options = { ...DEFAULT_OPTIONS, ...options }
 
 	// console.log( {eyeData, irisWidth,irisHeight, diameter, options })
 
@@ -51,14 +58,20 @@ options = { ...DEFAULT_OPTIONS, options }
 	{
 		// round no perspective...
 		canvasContext.beginPath()
-		canvasContext.fillStyle  = 'white'
-		canvasContext.arc(pupil[0], pupil[1], 4 * radius, 0, TAU)
+		canvasContext.arc(pupil[0], pupil[1], radius * options.scleraRadius, 0, TAU)
+		canvasContext.fillStyle  = options.sclera
 		canvasContext.fill()
 		canvasContext.closePath()
 
+		// IRIS - two different styles... 
+		// 1. Circular Eyes
+		// 2. Frank Sidebottom Eyes
 		canvasContext.beginPath()
 		canvasContext.fillStyle  = options.iris
-		canvasContext.arc(pupil[0], pupil[1], radius, 0, TAU)
+		canvasContext.arc(pupil[0], pupil[1], radius * options.irisRadius, 0, TAU * showRatio )
+		canvasContext.lineTo(pupil[0], pupil[1])
+		canvasContext.fill()
+		canvasContext.closePath()
 
 		// canvasContext.arcTo(up[0], up[1], inner[0], inner[1], options.irisRadius)
 		// canvasContext.arcTo(inner[0], inner[1], down[0], down[1], options.irisRadius)
@@ -68,23 +81,27 @@ options = { ...DEFAULT_OPTIONS, options }
 		//canvasContext.arc(pupil[0], pupil[1], radius * options.irisRadius, 0, TAU)
 		// canvasContext.ellipse(pupil[0], pupil[1], irisWidth, irisHeight, 0, 0, TAU)
 		// canvasContext.ellipse(pupil[0], pupil[1], irisHeight, irisWidth, 0, 0, TAU)
-		canvasContext.fill()
-		canvasContext.closePath()
-
+		
+		// PUPILS
 		// 1 + clamp( (10+iris[2]) * 0.8, 5, 10 )
 		canvasContext.beginPath()
 		canvasContext.fillStyle  = options.pupil
-		canvasContext.arc(pupil[0], pupil[1], radius * options.pupilRadius, 0, TAU)
+		canvasContext.arc(pupil[0], pupil[1], radius * options.pupilRadius, 0, TAU * showRatio)
 		canvasContext.fill()
+
 	}else{
+
+		// ---- EYES CLOSED -----
+
 		// draw cute triangle?
+		// and rotate accordingly
 		// FIXME: make sure they are left and right!
 		canvasContext.beginPath()
 		canvasContext.fillStyle = options.iris
 		canvasContext.fillRect( 
 			(!isLeft ? inner[0] : outer[0]), 
 			(!isLeft ? inner[1] : outer[1]), 
-			irisWidth * 4, 
+			radius * options.scleraRadius, 
 			irisHeight 
 		)
 
