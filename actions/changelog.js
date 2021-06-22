eb821b9a1ef48f6750ebc0b4315496bdba523798
designerzen <designerzen@gmail.com>
2021-06-22 14:10:59 +0100

    feat: Changelog now uses GIT commits

--- actions/changelog.js
+++ actions/changelog.js
@@ -1,6 +1,9 @@
 import Changelog from 'generate-changelog'
 import {readFileSync, writeFileSync} from 'fs'
+import showdown from 'showdown'
 
+const {Converter} = showdown
+const converter = new Converter()
 const packageFile = readFileSync('./package.json')
 const pkg = JSON.parse(packageFile)
 const packageVerson = pkg.version
@@ -17,5 +20,11 @@ Changelog.generate({
 		repoUrl: 'https://github.com/designerzen/interface' 
 	})
 	.then( (changelog) => {
+
+		// changelog is in markdown so convert to html?
+		const html = converter.makeHtml(changelog)
+
 		writeFileSync('./CHANGELOG.md', changelog)
+		writeFileSync('./source/changes.html', html )
+		// writeFileSync('./source/changes.html', `<section>${html}</section>` )
 	})
