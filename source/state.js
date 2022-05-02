d3fabe37bf1afc433164e7a2ffe08f3002231776
designerzen <designerzen@gmail.com>
2022-05-02 20:39:53 +0100

    update: External state

--- source/state.js
+++ source/state.js
@@ -1,28 +1,79 @@
-import { addToHistory } from './location-handler'
+/**
+ * This is a simple way to pass state from between pages without storing locally
+ * bypassing the need for cookies or local storage.
+ */
+
+import { addToHistory, getLocationSettings } from './location-handler'
 
 let state = null
 let main
 
+/**
+ * Adds classnames to the main element to indicate the current state
+ */
+ const setUIState = (key, value) => {
+	main.classList.toggle(`flag-${key}`, value )
+}
+
+/**
+ * Instantiates the state object
+ * @param {String} key Optional key to specify return
+ * @returns {Object} current state
+ */
+export const getState = ( key ) => {
+	return key ? state[key] : state
+}
+
+/**
+ * Instantiates the state object
+ * @param {Object} defaultOptions initial state to overwrite
+ * @param {HTMLElement} domElement HTML Element to add classes to
+ * @returns {Object} current state
+ */
 export const createState = ( initialState, domElement ) => {
-	state = Object.assign({},initialState )
+	state = Object.assign( {}, initialState )
 	main = domElement || document.documentElement
 	return state
 }
 
-///////////////////////////////////////////////////////////
-// simply refreshes the ui with any updated options
-export const refreshState = ()=>{
 
-	Object.entries(state).forEach(([key,value])=>{
-		main.classList.toggle(`flag-${key}`, value )
-	})
+/**
+ * Instantiates the state object
+ * @param {Object} defaultOptions initial state to overwrite
+ * @param {HTMLElement} domElement HTML Element to add classes to
+ * @returns {Object} current state
+ */
+export const loadState = ( defaultOptions, domElement ) => {
+	return createState( getLocationSettings(defaultOptions) , domElement )
 }
 
-///////////////////////////////////////////////////////////
-// This simply allows you to set the state of the ui
+
+/**
+ * refreshState : Refreshes the ui with any updated options
+ * Adds classes to the domElement with all of the correct
+ * classnames listed as option selection from the state
+ * @returns {Object} current state
+ */
+export const refreshState = ()=>{
+	Object.entries(state).forEach(([key,value])=>{
+		setUIState( key, value )
+	})
+	return state
+}
+
+/**
+ * setState : set / store the state of the ui & update DOM
+ * Allows you to set the state of the ui by passing in 
+ * individual options in key value pairs.
+ * This is used in realtime button actions for example
+ * @param {String} key state unique key ID
+ * @param {String} value value to set the key to
+ * @param {Array<HTMLElement>} elements buttons with toggleable attributes
+ * @param {Boolean} saveHistory add to localStorage cache
+ * @returns {Object} current state
+ */
 export const setState = ( key, value, elements=null, saveHistory=true )=>{
 	
-	// update object
 	state[key] = value
 	
 	if (saveHistory)
@@ -31,7 +82,7 @@ export const setState = ( key, value, elements=null, saveHistory=true )=>{
 	}
 
 	// set flag on main 
-	main.classList.toggle(`flag-${key}`, value )
+	setUIState( key, value )
 
 	// NB. only send elements where the element itself wasnt
 	// responsible for the origin of the event
@@ -46,8 +97,7 @@ export const setState = ( key, value, elements=null, saveHistory=true )=>{
 		{
 			elements[key].parentNode.classList.toggle("checked", value )
 		}
-		console.log( "Setting state", elements[key].checked , elements[key], {elements, ui, key} )
-		
+		//console.log( "Setting state", elements[key].checked , elements[key], {elements, ui, key} )
 	}
 
 	return state
