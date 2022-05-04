ee674fad5b8d656134e96af777525c82dfa5b24a
designerzen <designerzen@gmail.com>
2022-05-04 15:13:13 +0100

    update: holography classes

--- source/visual/3d.js
+++ source/visual/3d.js
@@ -1,11 +1,3 @@
-import * as HoloPlayCore from './node_modules/holoplay-core/dist/holoplaycore.module.js'
-
-// the holoplay renderer should act as your THREE.WebGLRenderer
-const renderer = new HoloPlayCore.Renderer()
-renderer.webglRenderer.shadowMap.enabled = true
-renderer.webglRenderer.shadowMap.autoUpdate = false
-renderer.webglRenderer.shadowMap.type = THREE.PCFSoftShadowMap
-
 // just a basic three.js scene, nothing special
 const scene = new THREE.Scene()
 scene.add( new THREE.AmbientLight( 0xcccccc, 0.4 ) )
@@ -27,11 +19,6 @@ backdrop.position.z = -0.5
 backdrop.receiveShadow = true
 scene.add(backdrop)
 
-// the holoplay camera should be used like a THREE.PerspectiveCamera
-const camera = new HoloPlayCore.Camera()
-camera.add( new THREE.PointLight( 0xffffff, 0.8 ) )
-// camera.lookAt( { x: videoWidth / 2, y: -videoHeight / 2, z: 0, isVector3: true } )
-
 // adding three cubes to the scene in different locations
 for (let i = 0; i < 3; i++) 
 {
@@ -42,42 +29,12 @@ for (let i = 0; i < 3; i++)
   scene.add(box)
 }
 
-// add the renderer's canvas to your web page (it will size to fill the page)
-document.body.appendChild(renderer.domElement)
+// // the update function gets called every frame, thanks to requestAnimationFrame()
+// export const updateLoookingGlass = (time) => {
+// 	//renderer.webglRenderer.shadowMap.needsUpdate = true;
+// 	// render() draws the scene, just like THREE.WebGLRenderer.render()
+// 	renderer.render(scene, camera)
 
-// the update function gets called every frame, thanks to requestAnimationFrame()
-export const updateLoookingGlass = (time) => {
-	//renderer.webglRenderer.shadowMap.needsUpdate = true;
-	// render() draws the scene, just like THREE.WebGLRenderer.render()
-	renderer.render(scene, camera)
-
-	requestAnimationFrame(updateLoookingGlass)
-}
-requestAnimationFrame(updateLoookingGlass)
-
-// HoloPlay.Buttons will emit events when the display's buttons are touched
-const buttons = new HoloPlay.Buttons()
-buttons.addEventListener('buttonPressed', (e) => {
-  switch (e.detail.name) 
-  {
-	case 'left':
-	  camera.rotation.y -= 0.002
-	  break
-
-	case 'right':
-	  camera.rotation.y += 0.002
-	  break
-
-	case 'square':
-	  if (camera.position.z > 0.2) {
-		camera.position.z -= 0.01
-	  }
-	  break
-
-	case 'circle':
-	  if (camera.position.z < 100) {
-		camera.position.z += 0.01
-	  }
-	  break
-  }
-})
+// 	requestAnimationFrame(updateLoookingGlass)
+// }
+// requestAnimationFrame(updateLoookingGlass)
