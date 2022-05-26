479dac6d02c88a0d4a09743dd9c5e5329dbd97da
designerzen <designerzen@gmail.com>
2022-05-26 17:54:47 +0100

    update: MIDI finalist fixes

---
+++ source/attractor.js
@@ -0,0 +1,13 @@
+/**
+ * Attract Mode! 
+ * This demos the app and allows for a player to totally immerse
+ * themselves in play without having to concern with actually
+ * interacting
+ */
+ export default class Attractor{
+
+	constructor( application ) {
+
+	}
+
+}
