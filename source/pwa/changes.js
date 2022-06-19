963dc44e9f94cc600525eae8a4333450548ce0fc
zen <designerzen@gmail.com>
2022-06-19 23:15:21 +0100

    update: Changelog loads only when updated

--- source/pwa/changes.js
+++ source/pwa/changes.js
@@ -1,11 +1,30 @@
 import changesPath from "url:../changes.html"
+import MANIFEST_PATH from "url:../manifest.webmanifest"
 
 export const fetchChangesAsText = async () => {
     const response = await fetch(changesPath)
     return await response.text()
 }
 
-export const injectChangeLog = async (id) => {
-	const contentDiv = document.getElementById(id)
-	contentDiv.innerHTML = await fetchChangesAsText()
+export const injectChangeLog = async (contentDiv) => {
+	const log = await fetchChangesAsText()
+	contentDiv.innerHTML = log
+}
+
+
+// Load the manifest file that has information about this release
+// and app
+export const getManifestData = async (manifestPath=MANIFEST_PATH) => {
+	try {
+	  const response = await fetch(manifestPath)
+	  const data = await response.json()
+	  if (data) 
+	  {
+		return data
+	  }
+
+	} catch (err) {
+		console.error("Manifest could not be loaded",err)
+		return null
+	}
 }
