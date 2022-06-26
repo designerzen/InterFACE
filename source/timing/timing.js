4ba2c1cc04376fd9b7433a0dd05dd268a3695d26
zen <designerzen@gmail.com>
2022-06-26 17:22:39 +0100

    update: Timing events shared

--- source/timing/timing.js
+++ source/timing/timing.js
@@ -13,14 +13,10 @@
 // SubFramesPerPPQN = SubFramesPerQuarterNote/TimeBase
 // MicrosPerPPQN = SubFramesPerPPQN * Frames * SubFrames
 
-export const CMD_START = "start"
-export const CMD_STOP  = "stop"
-export const CMD_UPDATE  = "update"
-
-export const EVENT_READY = "ready"
-export const EVENT_STARTING = "starting"
-export const EVENT_STOPPING = "stopping"
-export const EVENT_TICK = "tick"
+import {
+	CMD_START,CMD_STOP,CMD_UPDATE,
+	EVENT_READY, EVENT_STARTING, EVENT_STOPPING, EVENT_TICK
+} from './timing.events.js'
 
 export const MAX_BARS_ALLOWED = 32
 
