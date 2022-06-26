548a6de443abadc42ec7adfa5ab8e9d2384ec125
zen <designerzen@gmail.com>
2022-06-26 17:22:39 +0100

    update: Timing events shared

---
+++ source/timing/timing.events.js
@@ -0,0 +1,8 @@
+export const CMD_START = "start"
+export const CMD_STOP  = "stop"
+export const CMD_UPDATE  = "update"
+
+export const EVENT_READY = "ready"
+export const EVENT_STARTING = "starting"
+export const EVENT_STOPPING = "stopping"
+export const EVENT_TICK = "tick"
