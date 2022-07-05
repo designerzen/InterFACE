19048ec3b468fd6c2664d174c451b13429e3371e
zen <designerzen@gmail.com>
2022-07-05 14:08:50 +0100

    update: Added lemming to Error page

--- source/dom/errors.js
+++ source/dom/errors.js
@@ -1,16 +1,20 @@
 
-export const showError = (error, solution, fatal=false) => {
+import { showReloadButton } from '../dom/button'
 
-	document.documentElement.classList.add("failure")
-	document.documentElement.remove("loading")
-	document.getElementById("feedback").appendChild( createReloadButton(true) )
+export const showError = (error, solution, fatal=false) => {
+ 
+	const body = document.documentElement
+	body.classList.add("failure")
+	body.classList.remove("loading")
+
+	document.getElementById("feedback").appendChild( showReloadButton(true) )
 			
 	console.error("Could not load", error )
 	console.warn( "Consider:", solution )
-
+ 
 	// play lemmings sound effect...
 	let audio = new Audio()
-	audio.src = URL.createObjectURL("/assets/audio/lemmings.wav")
-	
+	audio.src = "/assets/audio/lemmings.wav"
+	audio.play()
 	// if fatal then we can't continue so show reload button?
 }
