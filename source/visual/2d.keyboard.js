6ed005462146672e276bce94646d16bd117b65ad
zen <designerzen@gmail.com>
2022-07-15 11:34:49 +0100

    update: New overlay keyboard

---
+++ source/visual/2d.keyboard.js
@@ -0,0 +1,182 @@
+import { RGBA_ASTC_4x4_Format } from 'three'
+import {drawRoundedRect} from './canvas'
+
+const whiteKeys = 7
+const blackKeys = 5
+const keyRadius = 2
+const BLACK_KEY_HEIGHT_SCALE = 0.5
+const BLACK_KEY_WIDTH_SCALE = 0.5
+
+const NO_KEY = 0
+const WHITE_KEY = 1
+const BLACK_KEY = 2
+const KEY_SEQUENCE = [ WHITE_KEY, BLACK_KEY, WHITE_KEY, BLACK_KEY, WHITE_KEY, NO_KEY, WHITE_KEY, BLACK_KEY, WHITE_KEY, BLACK_KEY, WHITE_KEY, BLACK_KEY, WHITE_KEY ]
+const DRAW_SEQUENCE = [ WHITE_KEY, BLACK_KEY ]
+
+const getKeyInSequence = (noteNumber) => KEY_SEQUENCE[ noteNumber % (KEY_SEQUENCE.length-1) ]
+
+
+class MusicalKey{
+	constructor(){
+
+	}
+
+	update(){
+
+	}
+}
+
+/**
+ * Just an onscreen canvas keyboard that changes colour when midi signal sent in
+ * It is actually two canvasses one with outlines over blocks of colour
+ */
+export default class MusicalKeyboard{
+
+	octaves = 1
+
+	context
+	buffer
+	originalKeyboard
+
+	get keyboardCanvas(){
+		return this.originalKeyboard
+	}
+
+	get canvas(){
+		return this.buffer
+	}
+
+	constructor( width, height, amountOfOctaves=1 ){
+		this.octaves = amountOfOctaves
+
+		// create custom double blit canvas
+		this.draw( width, height, amountOfOctaves )
+	}
+
+	drawKey( colour, noteNumber ){
+		const height = this.buffer.height
+		this.context.fillStyle = 'transparent'//colour === BLACK_KEY ? "black" : "white" 
+		const key = drawRoundedRect( 
+			this.context, 
+			this.keyPositions[noteNumber], 0, 
+			this.keyWidths[noteNumber], this.keyHeights[noteNumber],
+			keyRadius, 
+			true, true
+		)
+	}
+
+	drawActiveKey( noteColour, noteNumber, velocity ){
+		// * velocity as alpha / white blend
+		this.context.fillStyle = noteColour
+		const key = drawRoundedRect( 
+			this.context, 
+			this.keyPositions[noteNumber], 0, 
+			this.keyWidths[noteNumber], this.keyHeights[noteNumber],
+			keyRadius, 
+			true, false
+		)
+		console.error("drawActiveKey", {...arguments})
+	}
+
+
+	clearActiveKey( noteNumber ){
+		this.context.clearRect( 
+			this.keyPositions[noteNumber], 0,
+			this.keyWidths[noteNumber], this.keyHeights[noteNumber]
+		)
+	}
+
+	clear(){
+		this.context.drawImage(this.originalKeyboard, 0, 0)
+	}
+
+	draw( width, height, amountOfOctaves ){
+
+		const buffer = document.createElement('canvas')		
+		buffer.width = width
+		buffer.height = height
+
+		const context = buffer.getContext("2d")
+		
+		this.buffer = buffer
+		this.context = context
+
+		// create a sequence the length of how many octaves
+		const keySequence = new Array(amountOfOctaves).fill([...KEY_SEQUENCE]).flat()
+		//const colourSequence = (new Array(whiteKeys)).map( (k,i) => keySequence[i]))
+		const keyQuantity = amountOfOctaves * (whiteKeys ) // keySequence.length
+		const whiteKeyWidth = width / (keyQuantity + 1)
+		const blackKeyWidth = whiteKeyWidth * BLACK_KEY_WIDTH_SCALE
+		
+		this.whiteKeyWidth = whiteKeyWidth
+		this.blackKeyWidth = blackKeyWidth
+		
+		this.keyWidths = []
+		this.keyHeights = []
+
+		let xi = 0
+		this.keyPositions = keySequence.map( (key, index) => {
+			switch(key)
+			{
+				case WHITE_KEY: 
+					xi += whiteKeyWidth
+					this.keyWidths.push( whiteKeyWidth )
+					this.keyHeights.push( height )
+					return xi
+
+				case BLACK_KEY: 
+					this.keyWidths.push( blackKeyWidth  )
+					this.keyHeights.push( height * BLACK_KEY_HEIGHT_SCALE )
+					return xi +( whiteKeyWidth / 2 )+ (blackKeyWidth / 2)
+
+				case NO_KEY:
+				default: 
+					this.keyWidths.push( 0 )
+					this.keyHeights.push( 0 )
+					return -1
+			} 
+		})
+
+		context.strokeStyle = "black" 
+		DRAW_SEQUENCE.forEach( keyColour => {
+			this.keyPositions.forEach( (position,i) => {
+				const colour = keySequence[i]
+				if (colour === keyColour)
+				{
+					this.drawKey(colour, i)
+				}
+			})
+		})
+		
+		// allow us to return to the clean state
+		context.save()
+
+		//create a new canvas to save our perfect keyboard in
+		const newCanvas = document.createElement('canvas')
+		const spareContext = newCanvas.getContext('2d')
+		newCanvas.width = buffer.width
+		newCanvas.height = buffer.height
+		spareContext.drawImage(buffer, 0, 0)
+		this.originalKeyboard = newCanvas
+
+		// console.error("2d::KeyBoard", this)
+	}
+
+	// call every frame to redraw
+	redraw(){
+
+	}
+
+	// MIDI interface!
+	noteOn( noteNumber, velocity=1 ){
+		// new MusicalKey()
+		const colour = `#${(Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6)}`
+		this.drawActiveKey( colour, noteNumber, velocity )
+	}
+
+	noteOff( noteNumber, velocity=1 ){
+		//this.drawActiveKey( noteNumber, colour )
+		// this.clear()
+		this.clearActiveKey( noteNumber )
+	}
+}
--- source/visual/canvas.js
+++ source/visual/canvas.js
@@ -1,5 +1,5 @@
 
-export const canvas = document.querySelector("canvas")
+export const canvas = document.getElementById('interface') // document.querySelector("canvas")
 export const canvasContext = canvas.getContext('2d')
 
 let width = canvas.width
@@ -54,12 +54,67 @@ export const copyCanvasToClipboard = async () => canvas.toBlob(async (blob) => {
 	await navigator.clipboard.write([ new ClipboardItem({ [blob.type]:blob }) ])
 })
 
-export const drawElement = element => {
+
+export const drawElement = (element, x=0, y=0, flip=true) => {
 	
 	canvasContext.save()
-	canvasContext.translate(width, 0)
-	canvasContext.scale(-1, 1)
-	// draw data frame to canvas but invert horizontally?
-	canvasContext.drawImage(element , 0, 0)
+	// invert horizontally (mirror image)
+	if (flip){
+		canvasContext.translate(width, 0)
+		canvasContext.scale(-1, 1)
+	}
+	canvasContext.drawImage(element , x, y)
 	canvasContext.restore()
 }
+
+/**
+ * Draws a rounded rectangle using the current state of the canvas.
+ * If you omit the last three params, it will draw a rectangle
+ * outline with a 5 pixel border radius
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {Number} x The top left x coordinate
+ * @param {Number} y The top left y coordinate
+ * @param {Number} width The width of the rectangle
+ * @param {Number} height The height of the rectangle
+ * @param {Number} [radius = 5] The corner radius; It can also be an object 
+ *                 to specify different radii for corners
+ * @param {Number} [radius.tl = 0] Top left
+ * @param {Number} [radius.tr = 0] Top right
+ * @param {Number} [radius.br = 0] Bottom right
+ * @param {Number} [radius.bl = 0] Bottom left
+ * @param {Boolean} [fill = false] Whether to fill the rectangle.
+ * @param {Boolean} [stroke = true] Whether to stroke the rectangle.
+ */
+ export const drawRoundedRect = (
+	ctx,
+	x,
+	y,
+	width,
+	height,
+	radius = 5,
+	fill = false,
+	stroke = true
+  ) => {
+	if (typeof radius === 'number') {
+	  radius = {tl: radius, tr: radius, br: radius, bl: radius}
+	} else {
+	  radius = {...{tl: 0, tr: 0, br: 0, bl: 0}, ...radius}
+	}
+	ctx.beginPath()
+	ctx.moveTo(x + radius.tl, y)
+	ctx.lineTo(x + width - radius.tr, y)
+	ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr)
+	ctx.lineTo(x + width, y + height - radius.br)
+	ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height)
+	ctx.lineTo(x + radius.bl, y + height)
+	ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl)
+	ctx.lineTo(x, y + radius.tl)
+	ctx.quadraticCurveTo(x, y, x + radius.tl, y)
+	ctx.closePath()
+	if (fill) {
+	  ctx.fill()
+	}
+	if (stroke) {
+	  ctx.stroke()
+	}
+  }
