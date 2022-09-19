6dc0f425e5c138864c41f573ded6e8577f1463b2
zen <designerzen@gmail.com>
2022-09-19 23:43:54 +0100

    update: Altered draw tools for x,y as objects

---
+++ source/visual/2d.eyebrows.js
@@ -0,0 +1,18 @@
+import {canvasContext} from './canvas'
+import { clamp, TAU } from "../maths/maths"
+import PALETTE, { DEFAULT_COLOURS } from "../palette"
+
+
+const DEFAULT_OPTIONS = {
+
+}
+
+/**
+ * Draw an eyebrow onto the Canvas
+ * @param {*} keypoints 
+ * @param {*} isLeft 
+ * @param {*} options 
+ */
+export const drawEyebrows = (keypoints, isLeft=true, options=DEFAULT_OPTIONS) => {
+
+}
--- source/visual/2d.mouth.js
+++ source/visual/2d.mouth.js
@@ -2,23 +2,41 @@ import PALETTE, { DEFAULT_COLOURS } from "../palette"
 import { canvasContext } from './canvas'
 import {drawNode} from './2d'
 
-//////////////////////////////////////////////////////////////////////
-// Draw a Mouth onto the canvas
-//////////////////////////////////////////////////////////////////////
+/**
+ * Draw a Mouth onto the canvas directly from the prediction
+ * The style is first we outline the entire mouth
+ * then on sing we fade in the fill within
+ * 
+ * @param {*} prediction 
+ * @param {Object} palette 
+ * @param {Boolean} debug 
+ * @returns 
+ */
 export const drawMouth = ( prediction, palette={h:90,s:50,l:50,a:0.6}, debug=true ) => {
 	
-	const { annotations, mouthRange,mouthWidth, mouthOpen } = prediction
-	const { lipsUpperInner,lipsLowerInner} = annotations 
-	const lips = [lipsUpperInner, lipsLowerInner]
-	
+	const { annotations, mouthRange, mouthWidth, mouthOpen } = prediction
+	const {lips} = annotations
+	const lipsLength = lips.length
 	const colour = `hsla(${palette.h},${palette.s}%,${palette.l}%, ${palette.a})`
 	const colourDark = `hsla(${palette.h},${palette.s}%,10%, ${palette.a})`
 	
 // console.log("drawing mouth", {colour, colourDark})
 
 	// central piece of the mouth
-	const lipUpperMiddle = lipsUpperInner[5]
-	const lipLowerMiddle = lipsLowerInner[5]
+	const lipLength = Math.round(lipsLength/4)
+	const midLipLength = Math.round(lipLength/2)
+
+	const lipUpperLeft = lips[0]
+	const lipLowerLeft = lips[lipLength * 2]
+
+	const lipUpperMiddle = lips[lipLength + midLipLength]
+	const lipLowerMiddle = lips[midLipLength] // lips[27]
+	
+	const lipInnerUpperMiddle = lips[lipsLength - midLipLength - 1]
+	const lipInnerLowerMiddle = lips[lipLength * 2 + midLipLength]
+	
+	const lipUpperRight = lips[lipLength * 2]
+	const lipLowerRight = lips[lipsLength-1]
 
 	// kermit style mouth with gradient!
 	// const topGradient = ctx.createLinearGradient(0, 0, 0, lipVerticalOpening)
@@ -26,10 +44,10 @@ export const drawMouth = ( prediction, palette={h:90,s:50,l:50,a:0.6}, debug=tru
 	// topGradient.addColorStop(1, "white")
 
 	// how can we work out height of the mouth???
-	const mouthGradient = canvasContext.createLinearGradient(0, 0, mouthWidth, mouthRange )
-	mouthGradient.addColorStop(0, colour)
-	mouthGradient.addColorStop(0.5, colourDark )
-	mouthGradient.addColorStop(1, colour)
+	// const mouthGradient = canvasContext.createLinearGradient(0, 0, mouthWidth, mouthRange )
+	// mouthGradient.addColorStop(0, colour)
+	// mouthGradient.addColorStop(0.5, colourDark )
+	// mouthGradient.addColorStop(1, colour)
 
 	// canvasContext.beginPath()
 	// canvasContext.moveTo(lipsUpper[0][0], lipsUpper[0][1])
@@ -41,45 +59,70 @@ export const drawMouth = ( prediction, palette={h:90,s:50,l:50,a:0.6}, debug=tru
 	// 	canvasContext.lineTo(x, y)
 	// }	
 	// canvasContext.fill()
+
+
 	canvasContext.lineWidth = 2
+
 	canvasContext.strokeStyle = colourDark
 	canvasContext.beginPath()
-	canvasContext.moveTo(lipsUpperInner[0][0], lipsUpperInner[0][1])
-	canvasContext.fillStyle  = mouthGradient
+	canvasContext.moveTo( lips[0].x, lips[0].y )
+	// //canvasContext.fillStyle = mouthGradient
+	// //canvasContext.fillStyle = mouthGradient
 
 	// dual lips mode
-	for (let l = 0, t=lips.length; l < t; l++) {
-
+	for (let l = 1, t=lips.length; l < t; l++) 
+	{
 		const lip = lips[l]
-		
-		for (let i = 0, q=lip.length; i < q; i++) 
-		{
-			const x = lip[i][0]
-			const y = lip[i][1]
-	
-			canvasContext.lineTo(x, y)
-		}	
+		canvasContext.lineTo( lip.x, lip.y )
 	}
 
-	canvasContext.closePath()
-	canvasContext.fill()
+	//canvasContext.closePath()
+	// canvasContext.fill()
 	canvasContext.stroke( )
-
 
 
 	if (debug)
 	{
-		drawNode(lipLowerMiddle, lipUpperMiddle, 5)
+		// left and right!!!
+		//drawNode(lipUpperLeft, lipLowerLeft, 5, 0xff0000 )
+		
+		//drawNode(lipLowerMiddle, lipUpperMiddle, 5, 0x0000ff )
+		
+		// top bttom?
+		// drawNode(lipUpperRight, lipLowerRight, 5, 0x00ff00 )
+		// drawNode( lips[0], lips[lipLength], 5, 0xffff00 )
+		// drawNode( lips[lipLength+1], lips[lipLength*2], 5, 0x00ffff )
+		// drawNode( lips[lipLength*2], lips[lipsLength-1], 5, 0x0000ff )
+		
+		
+		// top to bottom
+		// drawNode( 
+		// 	lips[lipLength + midLipLength], 
+		// 	lips[midLipLength], 
+		// 	5, 
+		// 	0xff00ff 
+		// )
+
+		// // left edge to right edge
+		// drawNode( lips[0], lips[lipsLength-1], 5, 0x0000ff )
+		// drawNode( lipInnerUpperMiddle, lipInnerLowerMiddle, 5, 0x0000ff )
+		// drawNode( lipLowerRight, lipLowerRight, 5, 0x0000ff )
+		// drawNode( lipUpperRight, lipUpperLeft, 5, 0xff00ff )
+		
+		// // top to bottom
+		// drawNode( 
+		// 	lips[midLipLength], 
+		// 	lips[lipLength + midLipLength], 
+		// 	5, 
+		// 	0x0000ff 
+		// )
 	
 		// add text
-		
 		canvasContext.fillStyle = PALETTE.blue
 		canvasContext.font = "12px Oxanium"
 		canvasContext.textAlign = "center"
-		canvasContext.fillText(`${Math.floor(mouthRange)}px`, lipLowerMiddle[0], lipLowerMiddle[1] - 20 )
+		canvasContext.fillText(`${Math.floor(mouthRange)}px`, lipLowerMiddle.x, lipLowerMiddle.y - 20 )
 	}
-	canvasContext.stroke( )
-	canvasContext.lineWidth = 1
-	
+
 	return {lipUpperMiddle, lipLowerMiddle }
 }
