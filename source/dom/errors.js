d81d388013e106acdd61f5e4ad02f913473877a3
zen <designerzen@gmail.com>
2023-03-23 22:42:26 +0000

    update: DOM aria ux improvements

--- source/dom/errors.js
+++ source/dom/errors.js
@@ -4,17 +4,24 @@ import { showReloadButton } from '../dom/button'
 export const showError = (error, solution, fatal=false) => {
  
 	const body = document.documentElement
+
 	body.classList.add("failure")
 	body.classList.remove("loading")
 
-	document.getElementById("feedback").appendChild( showReloadButton(true) )
+	// TODO: show the error messages on screen in a dialog
+
+	if (fatal)
+	{
+		// if fatal then we can't continue so show reload button?	
+		// add a reload button to the feedback node
+		document.getElementById("feedback").appendChild( showReloadButton(true) )
 			
+		// play lemmings sound effect...
+		const audio = new Audio()
+		audio.src = "/assets/audio/lemmings.wav"
+		audio.play()
+	}
+
 	console.error("Could not load", error )
 	console.warn( "Consider:", solution )
- 
-	// play lemmings sound effect...
-	let audio = new Audio()
-	audio.src = "/assets/audio/lemmings.wav"
-	audio.play()
-	// if fatal then we can't continue so show reload button?
 }
