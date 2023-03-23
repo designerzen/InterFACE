87e1c8b3463a513894a7029902bef4ff9456374b
zen <designerzen@gmail.com>
2023-03-23 19:05:02 +0000

    fix: tapping and holding face improvements

--- source/dom/ui.js
+++ source/dom/ui.js
@@ -158,112 +158,6 @@ export const updateTempo = tempo =>{
 }
 
 /**
- * This is the 2nd screen, just after loading
- * @param {Object} options Configuration object
- * @returns {Boolean} true if the user hit duet
- */
-export const showPlayerSelector = (options) => new Promise( (resolve,reject)=>{
-
-	const CSS_CLASS = "player-selection"
-	const form = doc.getElementById("onboard")
-	const panel = doc.getElementById("player-selector")
-	const solo = panel.querySelector("#button-solo")
-	const duet = panel.querySelector("#button-duet")
-	const trio = panel.querySelector("#button-trio")
-	const start =  doc.getElementById("button-start")
-	const body = doc.documentElement
-	
-	const advanced = panel.querySelector("#toggle-advanced-mode")
-	const automation = panel.querySelector("#toggle-automation-mode")
-
-	// FIXME!
-	let advancedMode = options.advancedMode || false
-	let automationMode = options.automationMode || false
-	let players = options.duet ? 2 : 1
-
-	// set the query to this stae
-	advanced.setAttribute( "checked", !advancedMode )
-	
-	body.classList.toggle(CSS_CLASS, true)
-
-	const complete = result => {
-		
-		// if we are in solo mode
-		if (result < 2)
-		{
-			solo.classList.toggle( "hide", true)
-			duet.classList.toggle( "hide", false)
-			
-			main.classList.add("solo")
-		
-		}else{
-			
-			duet.classList.toggle( "hide", true)
-			solo.classList.toggle( "hide", false)
-			
-			main.classList.add("duet")
-		}
-
-		// start the animation out.
-		// NB. This is not superflous as the camera
-		// takes a broze age to load into memory
-		panel.classList.add("completed")
-		body.classList.toggle(CSS_CLASS, true)
-			
-		// wait for animation to complete
-		setTimeout( ()=> {
-			//console.log({advancedMode})
-			body.classList.toggle('advanced', advancedMode)
-			body.classList.toggle(CSS_CLASS, false)
-			panel.classList.remove("completed")
-		}, 45 )
-		
-		resolve({
-			players:result,
-			advancedMode,
-			automationMode
-		})
-	}
-
-	const setPlayers = amount => {
-		players = amount
-		body.classList.remove("solo", "duet")
-		body.classList.add( players === 1 ? "solo" : "duet")
-	}
-
-	solo.addEventListener("click", event => setPlayers(1) )
-	duet.addEventListener("click", event => setPlayers(2) )
-	trio.addEventListener("click", event => setPlayers(3) )
-
-	advanced.addEventListener("change", event =>{ 
-		advancedMode = !advancedMode 
-		body.classList.toggle("beginner", !advancedMode)
-	})
-	
-	automation.addEventListener("change", event =>{ 
-		automationMode = !automationMode 
-		body.classList.toggle("automated", automationMode)
-	})
-	
-	// start.addEventListener("click", event => {
-	// 	event.preventDefault()
-	// 	complete(players)
-	// 	return false
-	// } , true )
-	
-	form.addEventListener("submit", (event) => {
-		event.preventDefault()
-		complete(players)
-		return false
-	}, true)
-
-	// set defaults
-	body.classList.toggle("beginner", !advancedMode)
-	setPlayers(players)
-	panel.focus()
-})
-
-/**
  * DOM elements on main app screen
  * @param {Object} options Configuration object
  */
@@ -331,4 +225,41 @@ export const setupInterface = ( options ) => {
  */
 export const focusApp = ()=>{ 
 	
+}
+
+
+
+// showPersonalControlPanel( this.name, this.controls, this.instrumentName ) 
+export const showPersonalControlPanel = (playerName, controls, instrumentName) => {
+	
+	const isCurrentlyOpen = controls.classList.contains("showing",true)
+	// find active input field and focus
+	const active = document.getElementById(instrumentName)
+	if (active)
+	{
+		active.focus()
+	}else{
+		// send focus to form?
+		controls.focus()
+	}
+
+	console.log("SHOW Form", {active, isCurrentlyOpen})
+	// FIXME: Add aria-roles
+	controls.classList.toggle("showing",true)
+	document.documentElement.classList.toggle(`${playerName}-sidebar-showing`,true)
+
+	return true
+}
+
+export const hidePersonalControlPanel = (playerName, controls) => {
+	console.log("HIDE Form")
+	
+	//const inputs = this.controls.querySelectorAll('input')
+	//inputs.forEach( input => input.removeEventListener('change',  this.onInstrumentInput))
+	//this.controls.innerHTML = ''
+	controls.classList.toggle("showing",false)
+	document.documentElement.classList.toggle(`${playerName}-sidebar-showing`,false)
+	// setTimeout( ()=> this.controls.classList.toggle("showing",false), 303 )	
+
+	return false
 }
