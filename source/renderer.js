3624b5fe0fba4ae2bf80b110a199aa3425470d23
zen <designerzen@gmail.com>
2023-03-23 19:02:26 +0000

    fix: Electron basis

---
+++ source/renderer.js
@@ -0,0 +1,16 @@
+// ELECTRON SPECIFIC FILE //////////////////////////////////////
+
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+// Opens the image in the default image viewer and waits for the opened app to quit.
+
+// INSTALL 3RD PARTY APPS WITH NPM AND INCLUDE THEM HERE
+
+//require('./js/app.js');
+//const {ipcRenderer} = require('electron');
+
+// self executing function here
