c77f721e5df0ba0bb5cb7292263deb7693b949d5
zen <designerzen@gmail.com>
2023-03-24 20:28:38 +0000

    Updates

--- tf/register_all_kernels.d.ts
+++
@@ -1,2 +0,0 @@
-/// <amd-module name="@tensorflow/tfjs-backend-wasm/dist/register_all_kernels" />
-export {};
--- tf/version.d.ts
+++
@@ -1,4 +0,0 @@
-/** @license See the LICENSE file. */
-/// <amd-module name="@tensorflow/tfjs-backend-wasm/dist/version" />
-declare const version = "3.20.0";
-export { version };
