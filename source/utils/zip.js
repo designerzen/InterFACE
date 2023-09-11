0479a85f6e5abbb763ead2950ece11128cdf4887
zen <designerzen@gmail.com>
2023-09-11 17:38:15 +0100

    update: Zip Worker

--- source/utils/zip.js
+++ source/utils/zip.js
@@ -1,28 +1,48 @@
 // import Zlib from 'zlibjs'
 import { deflate, unzip } from './zlib'
 
-export const extractZip = (data) => new Promise( (resolve, reject) => {
+export const extractZip = (data, useWorker=false ) => new Promise( (resolve, reject) => {
 	
-	const zipped = new Uint8Array(data)
-	unzip(zipped, (err, buffer) => {
+	if (useWorker)
+	{
+		let worker = new Worker(
+			new	URL('./zip.worker.js', import.meta.url), 
+			{type: 'module'}
+		)
 
-		if (err) 
-		{
-		  	console.error('An error occurred:', err)
-			reject(err)
-
-		}else{
-
-			const xml = new TextDecoder("utf-8").decode(buffer)
-			resolve(xml)
+		worker.onmessage = message => {
+			//console.log("zippy", message)
+			resolve(message.data)
+			worker.terminate()
+			worker = null
 		}
-	})
+
+		worker.onerror = error => reject(error)
+
+		worker.postMessage({data})
+
+	}else{
+
+		const zipped = new Uint8Array(data)		
+		unzip(zipped, {}, (err, buffer) => {
+
+			if (err) 
+			{
+				console.error('An error occurred:', err)
+				reject(err)
+
+			}else{
+				resolve(buffer)
+			}
+		})	
+	}
 } )
+
 
 /**
  * TODO: Pass in some data to create a zip file...
  * @param {*} data 
- */
 export const createZip = data => {
 
 }
+ */
