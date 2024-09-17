4cb0b66e20140e77f8b5bc9825e3d90a76bf5982
zen <designerzen@gmail.com>
2024-09-17 20:24:28 +0100

    feat: shaders for 3d display

---
+++ source/display/shaders/3d.js
@@ -0,0 +1,32 @@
+export const VERTEX_SHADER = `
+uniform float pointMultiplier;
+
+attribute float size;
+attribute float angle;
+attribute vec4 colour;
+
+varying vec4 vColour;
+varying vec2 vAngle;
+
+void main() {
+  vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+
+  gl_Position = projectionMatrix * mvPosition;
+  gl_PointSize = size * pointMultiplier / gl_Position.w;
+
+  vAngle = vec2(cos(angle), sin(angle));
+  vColour = colour;
+}`
+
+
+export const FRAGMENT_SHADER = `
+
+uniform sampler2D diffuseTexture;
+
+varying vec4 vColour;
+varying vec2 vAngle;
+
+void main() {
+  vec2 coords = (gl_PointCoord - 0.5) * mat2(vAngle.x, vAngle.y, -vAngle.y, vAngle.x) + 0.5;
+  gl_FragColor = texture2D(diffuseTexture, coords) * vColour;
+}`
