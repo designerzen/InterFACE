1a95bafbe0b2bd2150690a43806c480d1bba48c6
zen <designerzen@gmail.com>
2024-09-17 15:48:18 +0100

    fix: actions for changelogging

--- actions/licenses.mjs
+++ actions/licenses.mjs
@@ -5,4 +5,17 @@
 // 300k of license data is loaded and hidden in scripts!
 
 // The LICENSE.md files are created by a package.json script
-// that calls a yarn command
+// that calls a yarn command then plays with the output
+
+import fs from 'fs'
+import path from 'path'
+
+const rawLicenses = fs.readFileSync(path.join(__dirname, '../LICENSE.md'), 'utf8')
+const licenseData = rawLicenses
+						.replaceAll('"""', '"')
+						.split('-----')
+
+const licenses = licenseData
+
+
+const intro = licenses.shift()
