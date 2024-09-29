34752835df9ac9bee9b42bdc2e3710c42b7c72d8
zen <designerzen@gmail.com>
2024-09-29 15:20:41 +0100

    fix: Safari compat tests

--- source/dom/compatability.js
+++ source/dom/compatability.js
@@ -1,7 +1,6 @@
 import { fetchPermissions, PERMISSION_GRANTED, PERMISSION_PROMPT, PERMISSION_UNAVAILABLE } from "../capabilities"
 import { filterVideoCameras } from "../hardware/camera"
 
-const UNAVAILABLE = "unavailable"
 /**
  * Update the table of compatability and reveal it
  * if the user has a device that will not work 100%
@@ -11,13 +10,14 @@ export const updateCapabalitiesTable = async (capabilities) => {
 	
 	const table = document.getElementById("compatability")
 	
+	const CHECKING = "checking"
+	const RESULT = "result"
+	const NOT_AVAILABLE = "not-available"
+	const AVAILABLE = "available"
+	const UNKNOWN = "unknown"
+
 	const updateTable = (permissions, cameras) => {
-
-		const CHECKING = "checking"
-		const RESULT = "result"
-		const NOT_AVAILABLE = "not-available"
-		const AVAILABLE = "available"
-
+		
 		let fatal = false
 	
 		// CAMERA --------------------------------------------------------------------------
@@ -29,7 +29,7 @@ export const updateCapabalitiesTable = async (capabilities) => {
 		if (!tableCamera || !cameraAvailability){
 			
 			fatal = true
-			cameraAvailability.classList.toggle(UNAVAILABLE, true)
+			cameraAvailability.classList.toggle(NOT_AVAILABLE, true)
 			cameraAvailability.classList.remove(CHECKING)
 			document.body.classList.toggle("camera-unavailable", true)
 			
@@ -40,7 +40,7 @@ export const updateCapabalitiesTable = async (capabilities) => {
 			document.body.classList.toggle("camera-unavailable", true)
 			// FIXME: instructions for how to enable permission
 			cameraAvailability.textContent = `Camera may not be available`
-			cameraAvailability.classList.toggle(UNAVAILABLE, true)
+			cameraAvailability.classList.toggle(NOT_AVAILABLE, true)
 			
 		} else if (
 			capabilities.cameraAvailable &&
@@ -58,7 +58,7 @@ export const updateCapabalitiesTable = async (capabilities) => {
 			fatal = true
 			document.body.classList.toggle("camera-unavailable", true)
 			cameraAvailability.textContent = `${cameras.length} Available`
-			cameraAvailability.classList.toggle(UNAVAILABLE, true)
+			cameraAvailability.classList.toggle(NOT_AVAILABLE, true)
 			console.warn("[FATAL] No camera available")
 		}	
 	
@@ -176,28 +176,43 @@ export const updateCapabalitiesTable = async (capabilities) => {
 		return fatal
 	}
 	
-	const permissions = await fetchPermissions()
-	let isFatalIssue = false
+	try{
+		const permissions = await fetchPermissions()
+		let isFatalIssue = false
+		
+		// FIXME: should we request the camera if permission was already granted?
+		// what is going on?
+		const devices = await navigator.mediaDevices.enumerateDevices()
+		const cameras = filterVideoCameras( devices )
+		if (cameras.length === 0)
+		{
+			// FATAL ERROR! No camera!
+			isFatalIssue = false
+			console.error("Compatability table", table )
+			console.error("Devices", {devices, cameras}) 
+			console.error("Permissions", {isFatalIssue, permissions, capabilities}) 
+		
+		}else{
+			isFatalIssue = updateTable(permissions, cameras)
+			// console.log("Compatability table", table )
+			// console.log("Devices", {devices, cameras}) 
+			// console.log("Permissions", {isFatalIssue, permissions, capabilities}) 
+		}
+		// console.info( )
 	
-	// FIXME: should we request the camera if permission was already granted?
-	// what is going on?
-	const devices = await navigator.mediaDevices.enumerateDevices()
-	const cameras = filterVideoCameras( devices )
-	if (cameras.length === 0)
-	{
-		// FATAL ERROR! No camera!
-		isFatalIssue = false
-		console.error("Compatability table", table )
-		console.error("Devices", {devices, cameras}) 
-		console.error("Permissions", {isFatalIssue, permissions, capabilities}) 
-	
-	}else{
-		isFatalIssue = updateTable(permissions, cameras)
-		// console.log("Compatability table", table )
-		// console.log("Devices", {devices, cameras}) 
-		// console.log("Permissions", {isFatalIssue, permissions, capabilities}) 
-	}
+		// now update the table based on permissions
+		return isFatalIssue
 
-	// now update the table based on permissions
-	return isFatalIssue
+	}catch(error){
+
+		console.info("Compat Table Error", error )
+		// remove all "checking" messages...
+		const checkingElements = document.querySelectorAll(CHECKING)
+		checkingElements.forEach( checkingElement => {
+			checkingElement.classList.remove(CHECKING) 
+			checkingElement.classList.toggle(UNKNOWN, true)
+		})
+		
+		return null
+	}
 }
