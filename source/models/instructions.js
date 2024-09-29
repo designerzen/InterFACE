057c0f30aca00a5fe8bf39d634947eb8442dc259
zen <designerzen@gmail.com>
2024-09-29 22:39:48 +0100

    feat: added special instructions for IEEE

--- source/models/instructions.js
+++ source/models/instructions.js
@@ -1,8 +1,13 @@
 // TODO: load appropriate language...
-import {QUICK_START, INSTRUCTIONS} from '../locales/en_GB'
+import {QUICK_START, INSTRUCTIONS, INSTRUCTION_FOR_AUTOMATION} from '../locales/en_GB'
+
+export const injectHelp = (index, ...data)=> QUICK_START.splice(index, 0, ...data)
+export const injectInstructions= (index, ...data)=> INSTRUCTIONS.splice(index, 0, ...data)
+export const injectInstructionsForAutomation= (index, ...data)=> INSTRUCTION_FOR_AUTOMATION.splice(index, 0, ...data)
 
 export const getHelp = (index, language="en-GB") => QUICK_START[index%(QUICK_START.length-1)]
 export const getInstruction = (index, language="en-GB") => INSTRUCTIONS[index%(INSTRUCTIONS.length-1)]
+export const getInstructionForAutomation = (index, language="en-GB") => INSTRUCTION_FOR_AUTOMATION[index%(INSTRUCTIONS.length-1)]
 
 // let instructionCount = 0
 // export const getNextInstruction = () => INSTRUCTIONS[(instructionCount+1)%(INSTRUCTIONS.length-2)]
