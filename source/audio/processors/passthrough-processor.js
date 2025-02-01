4b68d3c6f31dd15efa837b171c32df99120295a1
zen <designerzen@gmail.com>
2025-02-01 17:53:42 +0000

    feature: OSS processors

--- source/audio/processors/passthrough-processor.js
+++ source/audio/processors/passthrough-processor.js
@@ -1,19 +1,31 @@
+/**
+* A simple bypass node demo.
+*
+* @class BypassProcessor
+* @extends AudioWorkletProcessor
+*/
 class PassthroughProcessor extends AudioWorkletProcessor {
 
 	name = "PassthroughProcessor"
 
 	constructor() {
 		console.log("Processor created")
-	  super();
+	  	super()
 	}
   
-	process(inputList, outputList, parameters) {
+	process(inputs, outputs, parameters) {
 	  // Using the inputs (or not, as needed),
 	  // write the output into each of the outputs
-	  // …
-	  return true;
+	  // By default, the node has single input and output.
+	  const input = inputs[0]
+	  const output = outputs[0]
+   
+	  for (let channel = 0; channel < output.length; ++channel) {
+		output[channel].set(input[channel])
+	  }
+   
+	  return true
 	}
 }
   
-registerProcessor("passthrough-processor", PassthroughProcessor);
-console.log("Processor registered")
+registerProcessor("passthrough-processor", PassthroughProcessor)
