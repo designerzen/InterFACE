4110b3a00a1c22ac270a0b7f3d07a5f270df47d5
zen <designerzen@gmail.com>
2025-02-07 11:28:40 +0000

    feature: Worklet instruments

--- source/audio/instruments/worklets/sampler.worklet.js
+++ source/audio/instruments/worklets/sampler.worklet.js
@@ -1,114 +1,206 @@
-import { AudioWorkletProcessor } from "@webaudiomodules/api"
+// import { WORKLET_SAMPLER } from "../../processors/sample-processor"
 
-export const WORKLET_SAMPLER = "sampler-processor"
+// const DEFAULT_OPTIONS = {
+// 	numberOfInputs?: number;
+//     numberOfOutputs?: number;
+//     outputChannelCount?: number[];
+//     parameterData?: Record<string, number>;
+//     processorOptions?: any;
+// }
 
-class SampleAudioWorkletNode extends AudioWorkletProcessor  {
+import WORKLET_LOCATION from 'worklet:../../processors/sample-processor.js'
+
+export const WORKLET_ID_SAMPLER = "sampler-processor"
+
+const DEFAULT_OPTIONS = {
+	// numberOfInputs: 1,
+	// numberOfOutputs: 1,
+	// outputChannelCount: [2],
+	// parameterData: {
+	// 	gain: 0.5,
+	// 	pan: 0,
+	// 	frequency: 440.0,
+	// }
+}
+
+export default class SampleAudioWorkletNode extends AudioWorkletNode {
 
 	// Static getter to define AudioParam objects in this custom processor.
 	static get parameterDescriptors() {
-		return [{
-			name: 'myParam',
-			defaultValue: 0.707
-		}]
+		return [
+			{
+				name: "gain",
+				defaultValue: 0.5,
+				minValue: 0,
+				maxValue: 1
+			},
+			{
+				name: "pan",
+				defaultValue: 0,
+				minValue: -1,
+				maxValue: 1
+			},
+			{
+				name: "frequency",
+				defaultValue: 440.0,
+				minValue: 27.5,
+				maxValue: 4186.009
+			}
+		]
 	}
 
-	constructor(context) {
-		super()
+	/**
+     * @constructor
+     * @param {BaseAudioContext} context The associated BaseAudioContext.
+	 * @param {AudioWorkletNodeOptions} options User-supplied options for AudioWorkletNode.
+     */
+	constructor(context, workletId=WORKLET_ID_SAMPLER, options=DEFAULT_OPTIONS) {
+		// try{
+		// 	console.error("registering sampler worklet!", {WORKLET_LOCATION, worklets:context.audioWorklet} ) 
+		// 	context.audioWorklet.addModule( WORKLET_LOCATION ).then( r => {
+		// 		console.error("registered sampler worklet!", {WORKLET_LOCATION, worklets:context.audioWorklet} ) 
+		// 	})
+			
+		// }catch(e){
+		// 	//
+		// 	console.error("Already registered sampler worklet!", e)
+		// }
+		super(context, workletId, options)
 		this.port.onmessage = this.onmessage.bind(this)
-		this.port.postMessage('Hi!')
 	}
 
-	// Pass in the WAV data and 
+	post( data){
+		return this.port.postMessage(data)
+	}
+
+	/**
+	 * Pass in the WAV data AudioBuffers 
+	 * @param {Event} event 
+	 */
 	onmessage(event) {
 		// Handling data from the node.
-		// loadSample(event.data)
-		console.log("SampleAudioWorkletNode:", event.data)
+		// console.log("SampleAudioWorkletNode:", event.data)
 		// loadSample(event.data)
 		switch (event.data.type) {
-            case 'play':
+			case 'available':
+				console.log('[SampleAudioWorkletNode:Available]')
 				break
+
+			case 'load':
+				console.error("SampleAudioWorkletNode: Loading", event)
+				
+				break
+
+			case 'play':
+				break
+
+			default:
+				console.error("SampleAudioWorkletNode: Unknown message type", event)
 		}
 	}
 
-	async loadSample(path){
+	createTransferableBufferData(audioBuffer) {
+		return [
+		  {
+			sampleRate: audioBuffer.sampleRate,
+			duration: audioBuffer.duration,
+			bufferLength: audioBuffer.length,
+			numberOfChannels: audioBuffer.numberOfChannels,
+		  },
+		  audioBuffer.getChannelData(0),
+		  this.numberOfChannels > 1
+			? audioBuffer.getChannelData(1)
+			: audioBuffer.getChannelData(0)
+		];
+	  }
+
+	/**
+	 * 
+	 * @param {String} path 
+	 */
+	async loadAudioFromURL(path) {
+		const audioBuffer = await this.loadBufferFromURL(path)
+		// const arrayBuffer = await this.loadArrayBufferFromURL(path)
+		// const arrayBuffer8 = new Uint8Array(this.mem);
+		
+		const data = this.createTransferableBufferData(audioBuffer)
+		
+		this.post({
+			type:"load", 
+			data
+		})
+		console.log("SampleAudioWorkletNode: Loaded", { audioBuffer, data} )
+	}
+	
+	/**
+	 * 
+	 * @param {String} path 
+	 * @returns 
+	 */
+	async loadBufferFromURL(path) {
+		const arrayBuffer = await this.loadArrayBufferFromURL(path)
+		const audioBuffer = await this.createAudioBufferFromArrayBuffer(arrayBuffer)
+		return audioBuffer
+	}
+	
+	/**
+	 * 
+	 * @param {String} path 
+	 * @returns {TrackSource}
+	 */
+	async loadTrackSourceFromURL(path) {
+		const audioBuffer = await this.loadBufferFromURL(path)
+		const trackSource = this.createBufferSource(audioBuffer)
+		return trackSource
+	}
+
+	/**
+	 * 
+	 * @param {String} path 
+	 * @returns {ArrayBuffer}
+	 */
+	async loadArrayBufferFromURL(path) {
 		const response = await fetch(path)
 		const arrayBuffer = await response.arrayBuffer()
+		return arrayBuffer
+	}
+
+	/**
+	 * 
+	 * @param {ArrayBuffer} audioBuffer 
+	 * @returns {AudioBuffer}
+	 */
+	async createAudioBufferFromArrayBuffer(arrayBuffer) {
 		const audioBuffer = await this.context.decodeAudioData(arrayBuffer)
 		return audioBuffer
 	}
 
-	async createBuffer(audioBuffer)
-	{
+	/**
+	 * 
+	 * @param {AudioBuffer} audioBuffer 
+	 * @returns {BufferSource} BufferSource 
+	 */
+	async createBufferSource(audioBuffer) {
 		const trackSource = this.context.createBufferSource()
 		trackSource.buffer = audioBuffer
 		return trackSource
 	}
 
-	async play(audioBuffer, offset=0, velocity=128, options={ loop:false } ){
-		
-		let trackSource = this.createBuffer( audioBuffer )
-		
+	async play(audioBuffer, offset = 0, velocity = 128, options = { loop: false }) {
+
+		let trackSource = this.createBufferSource(audioBuffer)
+
 		trackSource.loop = options.loop || false
 
-		if (this.context.state === 'suspended') 
-		{
+		if (this.context.state === 'suspended') {
 			await this.context.resume()
 		}
-		
-		if (offset == 0) 
-		{
-			trackSource.start( 0 )
-		//offset = context.currentTime
+
+		if (offset == 0) {
+			trackSource.start(0)
+			//offset = context.currentTime
 		} else {
 			trackSource.start(0, this.context.currentTime - offset)
 		}
 	}
-
-	// Float32Array(128)
-	process(inputs, outputs, parameters) {
-		// The processor may have multiple inputs and outputs. Get the first input and
-		// output.
-		const input = inputs[0]
-		const output = outputs[0]
-
-		console.error("process", {inputs, outputs, parameters} )
-
-		// Each input or output may have multiple channels. Get the first channel.
-		const inputChannel0 = input[0]
-		const outputChannel0 = output[0]
-
-		// Get the parameter value array.
-		// |myParamValues| is a Float32Array of either 1 or 128 audio samples
-		// calculated by WebAudio engine from regular AudioParam operations.
-		// (automation methods, setter) Without any AudioParam change, this array
-		// would be a single value of 0.707.
-		const myParamValues = parameters.myParam || []
-
-		// if |myParam| has been a constant value during this render quantum, the
-		// length of the array would be 1.
-		if (myParamValues.length === 1) {
-
-			// |myParam| has been a constant value for the current render quantum,
-			// which can be accessed by |myParamValues[0]|.
-
-			// Simple gain (multiplication) processing over a render quantum
-			// (128 samples). This processor only supports the mono channel.
-			for (let i = 0; i < inputChannel0.length; ++i) {
-				outputChannel0[i] = inputChannel0[i] * myParamValues[0]
-			}
-
-		} else {
-
-			// |myParam| has been changed and |myParamValues| has 128 values.
-			for (let i = 0; i < inputChannel0.length; ++i) {
-				outputChannel0[i] = inputChannel0[i] * myParamValues[i]
-			}
-		}
-
-		// To keep this processor alive.
-		return true
-	}	
 }
-
-
-registerProcessor( WORKLET_SAMPLER , SampleAudioWorkletNode)
---
+++ source/audio/instruments/worklets/soundfont.worklet.js
@@ -0,0 +1,89 @@
+import { INSTRUMENT_DATA_PACK_OPEN_SF, INSTRUMENT_PACK_OPEN_SF } from "../../../settings/options.instruments"
+import SoundFont from "../../sound-font"
+import SampleAudioWorkletNode from "./sampler.worklet"
+
+export const WORKLET_ID_SOUNDFONT = "soundfont-processor"
+
+const DEFAULT_OPTIONS = {
+	// numberOfInputs: 1,
+	// numberOfOutputs: 1,
+	// outputChannelCount: [2],
+	// parameterData: {
+	// 	gain: 0.5,
+	// 	pan: 0,
+	// 	frequency: 440.0,
+	// }
+}
+
+export default class SoundFontAudioWorkletNode extends SampleAudioWorkletNode {
+
+	soundfont 
+	
+	constructor(context, options=DEFAULT_OPTIONS) {
+		super(context, WORKLET_ID_SOUNDFONT, options)
+		this.soundfont = new SoundFont( context )
+	}
+
+	createTransferableBufferDataArray(buffers){
+		
+		return buffers.map( element => {
+			const output = {meta:{}}
+			for (let e in element)
+			{
+				if (!element.hasOwnProperty(e)){
+					continue
+				}
+				switch(e)
+				{
+					case "name":
+					case "title":
+					case "family":
+						output.meta[e] = element[e]
+						break
+
+					default:
+						output[e] = this.createTransferableBufferData(element[e])
+				}
+			}
+
+			return output
+		})
+	}
+
+	async loadFont(path='./assets/audio/'){
+
+		const fontDescriptor = {
+			descriptor: INSTRUMENT_PACK_OPEN_SF, 
+			descriptorPath:path,
+			preset:1 
+		}
+
+		// load in a sound font - this can be either a fully qualified url
+		// a relative uri or the name of the pack
+		// const fontData = await this.soundfont.loadFont( this.instrumentPack, './audio/', p => onProgress && onProgress( p / 2 ) )
+		console.log("loadFont", fontDescriptor )
+		const fontData = await this.soundfont.load( fontDescriptor, ( p,i,l ) => this.onLoadProgress( p,i,l ) ) 
+		console.log("loadedFont", fontData )
+		// const buffers = await this.soundfont.loadAllPresets()
+		const presetNames = fontData[0].folder
+		console.log("presetNames", presetNames )
+		const buffers = await this.soundfont.loadPresets( presetNames, {},( p,i,l ) => this.onLoadProgress( p,i,l ) )
+		console.log("loadedbuffers", {buffers} )
+
+		const data = this.createTransferableBufferDataArray(buffers)
+		console.log("createTransferableBufferDataArray", data[0] )
+		this.post({type:"load", data:data[0] })
+
+		return buffers
+	}
+
+
+
+	async loadPreset( presetName, options={ }, onProgressCallback=null ){
+		return await this.soundfont.loadPreset( presetName, options, onProgressCallback )
+	}
+
+	onLoadProgress( progress, position, length ){
+		console.log("SoundFont:onLoadProgress", progress, {position, length} )
+	}
+}
