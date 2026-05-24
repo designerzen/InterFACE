023122c45629b449e435ea2458fb57e5192c2a19
zen <designerzen@gmail.com>
2026-05-24 10:08:31 +0100

    feat: subdivisions

--- source/settings/options.displays.js
+++ source/settings/options.displays.js
@@ -21,8 +21,21 @@ export const DEFAULT_OPTIONS_DISPLAY_WEBGL = {
 	showParticles:true,
 	// geometry subdivision for denser particles (0 = disabled, 1 = double density, 2 = 4x, etc)
 	geometrySubdivisions: 0,
+	showWalls: false,
+	adaptiveGeometrySubdivisions: true,
+	targetFrameRate: 60,
+	minimumStableFrameRate: 54,
+	frameRateSampleFrames: 90,
+	maximumDroppedFrameRatio: 0.1,
+	droppedFrameMultiplier: 1.5,
+	adaptiveSubdivisionCooldown: 1500,
 	// 3d model
 	showAvatar:false,
+	avatarOpacity:0.38,
+	avatarScaleMultiplier:1.35,
+	avatarYOffset:0,
+	avatarZOffset:0.055,
+	avatarAlignmentSmoothing:0.42,
 	blendShapes:true,
 	updateFaceButtonAfter:UPDATE_FACE_BUTTON_AFTER_FRAMES
 }
