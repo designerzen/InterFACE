d32dba895b61d91142fd0d112193caa6ff3ca645
zen <designerzen@gmail.com>
2026-06-07 13:40:51 +0100

    feat: advanced / basic instructions option

--- source/models/instructions.js
+++ source/models/instructions.js
@@ -1,56 +1,63 @@
 // TODO: load appropriate language...
-import {QUICK_START, INSTRUCTIONS, INSTRUCTION_FOR_AUTOMATION} from '../locales/en_GB.js'
+import { QUICK_START, INSTRUCTIONS, INSTRUCTION_FOR_AUTOMATION } from '../locales/en_GB.js'
 import { fetchJSON } from '../utils/fetch.js'
 
-const QUICK_START_GUIDE = [...QUICK_START]
-const INSTRUCTIONS_GUIDE = [...INSTRUCTIONS]
-const AUTOMATION_INSTRUCTIONS_GUIDE = [...INSTRUCTION_FOR_AUTOMATION]
+const getIndexedInstruction = (guide, index) => guide[index % (guide.length - 1)]
 
-export const injectHelp = (index, ...data)=> QUICK_START_GUIDE.splice(index, 0, ...data)
-export const injectInstructions= (index, ...data)=> INSTRUCTIONS_GUIDE.splice(index, 0, ...data)
-export const injectInstructionsForAutomation= (index, ...data)=> AUTOMATION_INSTRUCTIONS_GUIDE.splice(index, 0, ...data)
+const selectCustomInstructionsForMode = (instructionsData, advancedMode) => {
+	if (Array.isArray(instructionsData)) {
+		return instructionsData
+	}
 
-export const getHelp = (index, language="en-GB") => QUICK_START_GUIDE[index%(QUICK_START_GUIDE.length-1)]
-export const getInstruction = (index, language="en-GB") => INSTRUCTIONS_GUIDE[index%(INSTRUCTIONS_GUIDE.length-1)]
-export const getInstructionForAutomation = (index, language="en-GB") => AUTOMATION_INSTRUCTIONS_GUIDE[index%(AUTOMATION_INSTRUCTIONS_GUIDE.length-1)]
+	if (!instructionsData || typeof instructionsData !== 'object') {
+		return []
+	}
 
-let instructionCount = 0
-export const getNextInstruction = (language="en-GB") => INSTRUCTIONS_GUIDE[(instructionCount+1)%(INSTRUCTIONS_GUIDE.length-1)]
+	const preferredMode = advancedMode ? 'advanced' : 'basic'
+	const fallbackMode = advancedMode ? 'basic' : 'advanced'
+	const selectedInstructions = instructionsData[preferredMode] ?? instructionsData[fallbackMode] ?? []
+
+	return Array.isArray(selectedInstructions) ? selectedInstructions : []
+}
 
 // export const getNextInstruction = () => INSTRUCTIONS[(instructionCount+1)%(INSTRUCTIONS.length-2)]
 
-export const getInstructions = async (language="en", referer="") => {
-	
-	// TODO: Load in appropriate language
-	
-	try{
+export const getInstructions = async (language = 'en', referer = '', options = {}) => {
+	const { advancedMode = true } = options
+
+	const quickStartGuide = [...QUICK_START]
+	const instructionsGuide = [...INSTRUCTIONS]
+	const automationInstructionsGuide = [...INSTRUCTION_FOR_AUTOMATION]
+
+	try {
 		// Attempt to Lazy load
 		// Load in our instruction tool kit
-		if (referer.length)
-		{
-			const instructionLocation = `./locales/${language}/instructions` + (referer ? `-${referer}` : "")+".json"
+		if (referer.length) {
+			const instructionLocation = `./locales/${language}/instructions${referer ? `-${referer}` : ''}.json`
 			const instructionsData = await fetchJSON(instructionLocation)
-			
-			// save to local space
-			// if automated, we show different instructions!
-			injectHelp(0, ...instructionsData )
-			injectInstructions(0, ...instructionsData)
-			injectInstructionsForAutomation(0, ...instructionsData)
+			const customInstructions = selectCustomInstructionsForMode(instructionsData, advancedMode)
 
-			// console.error("CUSTOM Instructions Loaded", instructionsData)
+			if (customInstructions.length) {
+				quickStartGuide.unshift(...customInstructions)
+				instructionsGuide.unshift(...customInstructions)
+				automationInstructionsGuide.unshift(...customInstructions)
+			}
 		}
+
+		let instructionCount = 0
 
 		return {
-			getHelp,
-			getInstruction,
-			getNextInstruction,
-			getInstructionForAutomation,
-			getQuantityOfHelp:() => QUICK_START_GUIDE.length - 1,
-			getQuantityOfInstructions:() => INSTRUCTIONS_GUIDE.length - 1
+			getHelp: index => getIndexedInstruction(quickStartGuide, index),
+			getInstruction: index => getIndexedInstruction(instructionsGuide, index),
+			getNextInstruction: () => {
+				instructionCount += 1
+				return getIndexedInstruction(instructionsGuide, instructionCount)
+			},
+			getInstructionForAutomation: index => getIndexedInstruction(automationInstructionsGuide, index),
+			getQuantityOfHelp: () => quickStartGuide.length - 1,
+			getQuantityOfInstructions: () => instructionsGuide.length - 1
 		}
-
-	}catch(error){
-				
+	} catch (error) {
 		// backup plan for failed JS loads
 		// getInstruction = getHelp = i => ''
 	}
